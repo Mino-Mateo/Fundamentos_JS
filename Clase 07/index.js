43f31e92e7c54d6209958e76d61b7e66714c68da
Mateo <minora.mateo@gmail.com>
2023-11-26 21:40:53 -0500

    Local Storage - Modulos

---
+++ Clase 07/index.js
@@ -0,0 +1,20 @@
+// // COMMON JS
+// // const {login, LiKES }= require('./module,')
+
+
+// //ES MODULES
+// import {login, LIKES} from './module.js'
+
+// login('Peter')
+// console.log(LIKES)
+
+//import {convertirFecha, convertirMoneda, conexionBDD} from './helpers.js'
+
+// const convertirFecha = () => { console.log("Moneda Convertida") }
+
+// const convertirMoneda = () => { console.log("Moneda Convertida") }
+
+// const conexionBDD = "conexion/300/user/password"
+
+
+
