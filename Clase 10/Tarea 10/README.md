a1a21e291c3040795055a3eb7244eaa9dbb7d3a3
Mateo <minora.mateo@gmail.com>
2023-12-06 19:13:59 -0500

    MonogDB - Javascript

---
+++ Clase 10/Tarea 10/README.md
@@ -0,0 +1,10 @@
+Conexion base de datos MongoDB
+
+Requiere del uso de las librerias de
+
+-   Mongoose
+-   Express
+
+Para su uso
+
+[![1.png](https://i.postimg.cc/rs3XpDwT/1.png)](https://postimg.cc/1VGYC5F7)
---
+++ Clase 10/Tarea 10/src/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const port = 8080;
+
+const username = "12345";
+const password = "12345";
+
+const conn_str = `mongodb+srv://${username}:${password}@clusterweb.klfzh9s.mongodb.net/?retryWrites=true&w=majority`;
+
+mongoose
+	.connect(conn_str)
+	.then(() => {
+		console.log("MongoDB está conectado exitosamente");
+		app.listen(port, () => {
+			console.log("Iniciando el servidor en el puerto", port);
+		});
+	})
+	.catch((err) => {
+		console.error("Error en la conexión a MongoDB:", err);
+	});
